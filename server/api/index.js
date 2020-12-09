2ff105ce8f42dfdd9566745635735295c457ebb8
Alexandros Georgantas <alexgeorg86@gmail.com>
2020-12-09 19:59:24 +0200

    fix(service): docker

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,8 @@
+const { authenticate } = require('@coko/service-auth')
 const epubChecker = require('./epuchecker.controller')
+const { uploadHandler } = require('./helpers')
 
-module.exports = epubChecker
+const backend = app => {
+  app.post('/api/epubchecker', authenticate, uploadHandler, epubChecker)
+}
+module.exports = backend
